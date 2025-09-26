551eb17860f14c222ef723febc90ff7650960522
Nguyen, Huy <hnguyen499@gatech.edu>
2025-09-26 16:16:37 -0400

    Fix Firebase configuration and remove conflicting files

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/super-tic-tac-toe/',
-  build: {
-    rollupOptions: {
-      input: 'index-react.html'
-    }
-  },
-  server: {
-    open: 'index-react.html'
-  }
+  base: '/super-tic-tac-toe/'
 })
