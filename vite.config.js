f688d878cc85d4029098b2879e67e14fb001315d
Nguyen, Huy <hnguyen499@gatech.edu>
2025-09-26 15:57:15 -0400

    Configure project for GitHub Pages hosting

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/super-tic-tac-toe/',
   build: {
     rollupOptions: {
       input: 'index-react.html'
